b9b63274257323324bf6ded3ead366f219537925
Erik Shaver <shaver.eriks@gmail.com>
2026-04-03 20:40:24 -0700

    Firebase config is now in a file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyDlLvjnCzhAuTcR3Z9buf7jf2qIQThOi7A",
+    authDomain: "wavelength-9a01a.firebaseapp.com",
+    projectId: "wavelength-9a01a",
+    storageBucket: "wavelength-9a01a.firebasestorage.app",
+    messagingSenderId: "147539645167",
+    appId: "1:147539645167:web:7d92d46ebf6ecf4bee011a",
+    measurementId: "G-QQPSJKWNJQ"
+};
